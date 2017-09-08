2314aca3a8da8f120507e8dfa479138d06e4e471
Andrew Low <andrew.low@inspection.gc.ca>
2017-09-08 14:29:02 -0400

    Code cleanup relatively complete, in New_Detector.py

--- New_Detector.py
+++ New_Detector.py
@@ -1,8 +1,14 @@
 from contam_object import ContamObject
+from accessoryFunctions.accessoryFunctions import printtime
+import time
+import multiprocessing
+import shutil
+import pysam
 import subprocess
 import argparse
 import glob
 import os
+from Bio.Blast.Applications import NcbiblastnCommandline
 
 
 class Detector(object):
@@ -10,9 +16,12 @@ class Detector(object):
         self.fastq_directory = args.fastq_directory
         self.samples = dict()
         self.tmpdir = args.output_name + 'tmp/'
+        self.outfile = args.output_name + '.csv'
+        self.logfile = args.output_name + '.log'
         self.database = args.rmlst_database
         if not os.path.exists(self.tmpdir):
             os.makedirs(self.tmpdir)
+        self.threads = args.threads
 
     def parse_fastq_directory(self):
         fastq_files = glob.glob(self.fastq_directory + '/*.f*q*')
@@ -38,65 +47,74 @@ class Detector(object):
         for sample in self.samples:
             # This captures paired reads.
             if self.samples[sample].reverse_reads != 'NA':
-                self.samples[sample].trimmed_forward_reads = self.tmpdir + self.samples[sample].forward_reads.split('/')[-1] + '_trimmed'
-                self.samples[sample].trimmed_reverse_reads = self.tmpdir + self.samples[sample].reverse_reads.split('/')[-1] + '_trimmed'
+                self.samples[sample].trimmed_forward_reads = self.samples[sample].forward_rmlst_reads + '_trimmed'
+                self.samples[sample].trimmed_reverse_reads = self.samples[sample].reverse_rmlst_reads + '_trimmed'
                 cmd = 'bbduk.sh in1={} in2={} out1={} out2={} qtrim=w trimq=20 k=25 minlength=50 forcetrimleft=15' \
-                  ' ref={}/resources/adapters.fa overwrite hdist=1 tpe tbo'.format(self.samples[sample].forward_reads,
-                                                                                   self.samples[sample].reverse_reads,
+                  ' ref={}/resources/adapters.fa overwrite hdist=1 tpe tbo threads={}'.format(self.samples[sample].forward_rmlst_reads,
+                                                                                   self.samples[sample].reverse_rmlst_reads,
                                                                                    self.samples[sample].trimmed_forward_reads,
                                                                                    self.samples[sample].trimmed_reverse_reads,
-                                                                                   bbduk_dir)
-                subprocess.call(cmd, shell=True)
+                                                                                   bbduk_dir, str(self.threads))
             else:
-                self.samples[sample].trimmed_forward_reads = self.tmpdir + self.samples[sample].forward_reads.split('/')[-1] + '_trimmed'
+                self.samples[sample].trimmed_forward_reads = self.samples[sample].forward_rmlst_reads + '_trimmed'
                 cmd = 'bbduk.sh in={} out={} qtrim=w trimq=20 k=25 minlength=50 forcetrimleft=15' \
-                      ' ref={}/resources/adapters.fa overwrite hdist=1 tpe tbo'.format(self.samples[sample].forward_reads,
+                      ' ref={}/resources/adapters.fa overwrite hdist=1 tpe tbo threads={}'.format(self.samples[sample].forward_rmlst_reads,
                                                                                        self.samples[sample].trimmed_forward_reads,
-                                                                                       bbduk_dir)
-                subprocess.call(cmd, shell=True)
+                                                                                       bbduk_dir, str(self.threads))
+            with open(self.logfile, 'a+') as logfile:
+                subprocess.call(cmd, shell=True, stderr=logfile)
 
     def extract_rmlst_reads(self):
         for sample in self.samples:
-            if self.samples[sample].trimmed_reverse_reads != 'NA':
-                self.samples[sample].forward_rmlst_reads = self.samples[sample].trimmed_forward_reads + '_rmlst'
-                self.samples[sample].reverse_rmlst_reads = self.samples[sample].trimmed_reverse_reads + '_rmlst'
-                cmd = 'bbduk.sh ref={} in1={} in2={} outm={} outm2={}'.format(self.database,
-                                                                              self.samples[sample].trimmed_forward_reads,
-                                                                              self.samples[sample].trimmed_reverse_reads,
+            if self.samples[sample].reverse_reads != 'NA':
+                self.samples[sample].forward_rmlst_reads = self.tmpdir + self.samples[sample].forward_reads.split('/')[-1] + '_rmlst'
+                self.samples[sample].reverse_rmlst_reads = self.tmpdir + self.samples[sample].reverse_reads.split('/')[-1] + '_rmlst'
+                cmd = 'bbduk.sh ref={} in1={} in2={} outm={} outm2={} threads={}'.format(self.database,
+                                                                              self.samples[sample].forward_reads,
+                                                                              self.samples[sample].reverse_reads,
                                                                               self.samples[sample].forward_rmlst_reads,
-                                                                              self.samples[sample].reverse_rmlst_reads)
-                subprocess.call(cmd, shell=True)
+                                                                              self.samples[sample].reverse_rmlst_reads,
+                                                                                         str(self.threads))
             else:
-                self.samples[sample].forward_rmlst_reads = self.samples[sample].trimmed_forward_reads + '_rmlst'
-                cmd = 'bbduk.sh ref={} in={} outm={} '.format(self.database,
-                                                              self.samples[sample].trimmed_forward_reads,
-                                                              self.samples[sample].forward_rmlst_reads)
-                subprocess.call(cmd, shell=True)
+                self.samples[sample].forward_rmlst_reads = self.tmpdir + self.samples[sample].forward_reads.split('/')[-1] + '_rmlst'
+                cmd = 'bbduk.sh ref={} in={} outm={} threads={}'.format(self.database,
+                                                              self.samples[sample].forward_reads,
+                                                              self.samples[sample].forward_rmlst_reads,
+                                                                        str(self.threads))
+            with open(self.logfile, 'a+') as logfile:
+                subprocess.call(cmd, shell=True, stderr=logfile)
 
     def subsample_reads(self):
         for sample in self.samples:
-            if self.samples[sample].reverse_rmlst_reads != 'NA':
-                self.samples[sample].subsampled_forward = self.samples[sample].forward_rmlst_reads + '_subsampled'
-                self.samples[sample].subsampled_reverse = self.samples[sample].reverse_rmlst_reads + '_subsampled'
-                cmd = 'reformat.sh in1={} in2={} out1={} out2={} overwrite samplebasestarget=700000'.format(self.samples[sample].forward_rmlst_reads,
-                                                                                                            self.samples[sample].reverse_rmlst_reads,
+            if self.samples[sample].trimmed_reverse_reads != 'NA':
+                self.samples[sample].subsampled_forward = self.samples[sample].trimmed_forward_reads + '_subsampled'
+                self.samples[sample].subsampled_reverse = self.samples[sample].trimmed_reverse_reads + '_subsampled'
+                cmd = 'reformat.sh in1={} in2={} out1={} out2={} overwrite samplebasestarget=700000'.format(self.samples[sample].trimmed_forward_reads,
+                                                                                                            self.samples[sample].trimmed_reverse_reads,
                                                                                                             self.samples[sample].subsampled_forward,
                                                                                                             self.samples[sample].subsampled_reverse)
-                subprocess.call(cmd, shell=True)
             else:
-                self.samples[sample].subsampled_forward = self.samples[sample].forward_rmlst_reads + '_subsampled'
-                cmd = 'reformat.sh in={} out={} overwrite samplebasestarget=700000'.format(self.samples[sample].forward_rmlst_reads,
+                self.samples[sample].subsampled_forward = self.samples[sample].trimmed_forward_reads + '_subsampled'
+                cmd = 'reformat.sh in={} out={} overwrite samplebasestarget=700000'.format(self.samples[sample].trimmed_forward_reads,
                                                                                            self.samples[sample].subsampled_forward)
-                subprocess.call(cmd, shell=True)
+            with open(self.logfile, 'a+') as logfile:
+                subprocess.call(cmd, shell=True, stderr=logfile)
 
     def run_jellyfish(self):
         for sample in self.samples:
             self.samples[sample].jellyfish_file = self.tmpdir + self.samples[sample].forward_reads.split('/')[-1] + '_jellyfish'
             # TODO: Add in threading options. Accomodate non-paired reads.
-            cmd = 'jellyfish count -m 31 -s 100M --bf-size 100M -t 12 -C -F 2 {} {} -o {}'.format(self.samples[sample].subsampled_forward,
-                                                                                                  self.samples[sample].subsampled_reverse,
-                                                                                                  self.samples[sample].jellyfish_file)
-            subprocess.call(cmd, shell=True)
+            if self.samples[sample].subsampled_reverse != 'NA':
+                cmd = 'jellyfish count -m 31 -s 100M --bf-size 100M -C -F 2 {} {} -o {} -t {}'.format(self.samples[sample].subsampled_forward,
+                                                                                                      self.samples[sample].subsampled_reverse,
+                                                                                                      self.samples[sample].jellyfish_file,
+                                                                                                      str(self.threads))
+            else:
+                cmd = 'jellyfish count -m 31 -s 100M --bf-size 100M -C -F 1 {} -o {} -t {}'.format(self.samples[sample].subsampled_forward,
+                                                                                                   self.samples[sample].jellyfish_file,
+                                                                                                   str(self.threads))
+            with open(self.logfile, 'a+') as logfile:
+                subprocess.call(cmd, shell=True, stderr=logfile)
 
     def write_mer_file(self):
         for sample in self.samples:
@@ -126,24 +144,105 @@ class Detector(object):
     def run_bbmap(self):
         for sample in self.samples:
             self.samples[sample].samfile = self.samples[sample].mer_fasta.replace('.fasta', '.sam')
-            cmd = 'bbmap.sh ref={} in={} outm={} overwrite subfilter=1 insfilter=0delfilter=0 indelfilter=0 ' \
-                  'nodisk'.format(self.samples[sample].mer_fasta, self.samples[sample].mer_fasta,
-                                  self.samples[sample].samfile)
-            subprocess.call(cmd, shell=True)
+            cmd = 'bbmap.sh ref={} in={} outm={} overwrite ambig=all ' \
+                  'nodisk threads={}'.format(self.samples[sample].mer_fasta, self.samples[sample].mer_fasta,
+                                             self.samples[sample].samfile, str(self.threads))
+            with open(self.logfile, 'a+') as logfile:
+                subprocess.call(cmd, shell=True, stderr=logfile)
+
+    def read_samfile(self):
+        for sample in self.samples:
+            f = open(self.samples[sample].mer_fasta)
+            mers = f.readlines()
+            f.close()
+            mer_dict = dict()
+            for i in range(0, len(mers), 2):
+                key = mers[i].replace('>', '')
+                key = key.replace('\n', '')
+                mer_dict[key] = mers[i + 1]
+            i = 0
+            try:
+                samfile = pysam.AlignmentFile(self.samples[sample].samfile, 'r')
+                for match in samfile:
+                    if "1X" in match.cigarstring and match.query_alignment_length == 31:
+                        reference = samfile.getrname(match.reference_id)
+                        if self.present_in_db(mer_dict[reference]):
+                            i += 1
+                if self.samples[sample].snv_count < i:
+                    self.samples[sample].snv_count = i
+            except OSError:
+                pass
+
+    def make_db(self):
+        db_files = ['.nhr', '.nin', '.nsq']
+        db_present = True
+        for db_file in db_files:
+            if not os.path.isfile(self.database + db_file):
+                db_present = False
+        if not db_present:
+            print('Making database!')
+            cmd = 'makeblastdb -dbtype nucl -in ' + self.database
+            with open(self.logfile, 'a+') as logfile:
+                subprocess.call(cmd, shell=True, stderr=logfile)
+
+    def present_in_db(self, query_sequence):
+        """
+        Checks if a sequence is present in our rMLST database, as some overhangs on reads can be in repetitive regions
+        that could cause false positives and should therfore be screened out.
+        :param query_sequence: nucleotide sequence, as a string.
+        :return: True if sequence is found in rMLST database, False if it isn't.
+        """
+        # Check if the db is there, and if not, make it.
+        self.make_db()
+        # Blast the sequence against our database.
+        blastn = NcbiblastnCommandline(db=self.database, outfmt=6)
+        stdout, stderr = blastn(stdin=query_sequence)
+        # If there's any result, the sequence is present. No result means not present.
+        if stdout:
+            return True
+        else:
+            return False
+
+    def write_output(self):
+        f = open(self.outfile, 'w')
+        f.write('Sample,NumContamSNVs,NumUniqueKmers,ContamStatus\n')
+        for sample in self.samples:
+            if self.samples[sample].snv_count > 1 or self.samples[sample].unique_kmers > 50000:
+                contam_status = 'Contaminated'
+            else:
+                contam_status = 'Clean'
+            f.write('{},{},{},{}\n'.format(sample.split('/')[-1], str(self.samples[sample].snv_count), str(self.samples[sample].unique_kmers),
+                                                                      contam_status))
+        f.close()
+
+    def cleanup(self):
+        shutil.rmtree(self.tmpdir)
 
 
 if __name__ == '__main__':
+    start = time.time()
     parser = argparse.ArgumentParser()
     parser.add_argument("fastq_directory", help="Folder that contains fastq files you want to check for contamination. "
                                                 "Will find any fastq file that contains .fq or .fastq in the filename.")
     parser.add_argument('output_name', help='Base name for output/temporary directories.')
     parser.add_argument('rmlst_database', help='rMLST database, in fasta format.')
+    cpu_count = multiprocessing.cpu_count()
+    parser.add_argument('-t', '--threads', type=int, default=cpu_count, help='Number of threads to run analysis with.')
     arguments = parser.parse_args()
     detector = Detector(arguments)
     Detector.parse_fastq_directory(detector)
-    Detector.quality_trim_reads(detector)
+    printtime('Extracting rMLST reads...', start)
     Detector.extract_rmlst_reads(detector)
-    Detector.subsample_reads(detector)
-    Detector.run_jellyfish(detector)
-    Detector.write_mer_file(detector)
-    Detector.run_bbmap(detector)
+    printtime('Quality trimming reads...', start)
+    Detector.quality_trim_reads(detector)
+    for i in range(5):
+        printtime('Working on subsampling {} of 5'.format(str(i + 1)), start)
+        Detector.subsample_reads(detector)
+        Detector.run_jellyfish(detector)
+        Detector.write_mer_file(detector)
+        Detector.run_bbmap(detector)
+        Detector.read_samfile(detector)
+    printtime('Writing output and cleaning up temporary files!', start)
+    Detector.write_output(detector)
+    Detector.cleanup(detector)
+    printtime('Contamination detection complete! :D', start)
--- README.md
+++ README.md
@@ -4,38 +4,27 @@
 - bbmap (>= 37.23) installed and present on your $PATH
 - jellyfish (>= 2.2.6) installed and on your $PATH
 - Python 3.5 (2.7 should also work, though that isn't tested.)
-- CLARK (>=1.2.3) installed and on your $PATH
+- NCBI BLAST+ (>=2.2.31) 
 
 ### Python Package Requirements
 - pysam >= 0.11.2.2
-- OLCTools >= 0.1.16
+- OLCTools >= 0.2.3
 
 ### Usage
 - Program takes a folder with paired or single-ended fastq files as input. Files can be uncompressed, or compressed with gzip/bzip2.
 - outputs results to a csv file which you name when calling the script.
-- Currently runs through Detector.py
+- Currently runs through New_Detector.py
 
 #### Options
-- Classify (-c): If the -c flag is added, when suspected cross-species contamination is found CLARK will be run (light version) to try to identify which species are present. Default=False.
 - Threads (-t): Number of threads to run analysis on. Default is number of cores on your system.
-- Trim_fastq (-tr): Performs quality trimming using bbduk. Off by default, but should probably almost always be turned on.
-- Remove\_bad_reads (-x): Removes reads which have contaminating kmers in them. Still highly experimental, not sure if it
-actually improves downstream assemblies at all.
 
 #### Example Usages
 
-Detect contamination on any fastq files within fastq folder, output results to outputname.csv
+Detect contamination on any fastq files within fastq folder, outputs results to outputname.csv, and uses database.fasta
+as the database of rMLST genes.
 
-`python3 Detector.py Fastq_Folder outputname.csv`
-
-Before detecting contamination, quality trim reads (highly recommended!)
-
-`python3 Detector.py Fastq_Folder outputname.csv -tr`
-
-Use CLARK to try and classify reads in samples that are suspected to have multiple species.
-
-`python3 Detector.py Fastq_Folder outputname.csv -c`
+`python3 Detector.py Fastq_Folder outputname database.fasta`
 
 ## Using Docker
-- A docker image has also been created for ease of use. It can be downloaded at (insert here eventually where you'll put it).
+- A docker image will be created for ease of use. It can be downloaded at (insert here eventually where you'll put it).
 - Add in instructions on mounting the files you want and running the docker image here.
