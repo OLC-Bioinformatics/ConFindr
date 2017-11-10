cd4ad8d679799d549a64a8622d6a75136f1e41db
adamkoziol <adam.koziol@inspection.gc.ca>
2017-11-10 08:30:29 -0500

    Created pipeline_contamination_detection.py to allow pipeline integration

---
+++ setup.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+from setuptools import setup
+__author__ = 'lowandrew'
+setup(
+    name="ConFinder",
+    version="0.0.1",
+    include_package_data=True,
+    license='MIT',
+    author='Andrew Low',
+    author_email='andrew.low@inspection.gc.ca',
+    description='Contamination finding',
+    url='https://github.com/lowandrew/KmerContam',
+    long_description=open('README.md').read()
+)
